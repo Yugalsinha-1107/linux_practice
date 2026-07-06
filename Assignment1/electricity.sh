e99ad8a0b46cf8e0591c2a6acda6ea8cc098b74b
Yugalsinha-1107 <143154932+Yugalsinha-1107@users.noreply.github.com>
2026-07-06 10:12:00 +0000

    added file

---
+++ Assignment1/electricity.sh
@@ -0,0 +1,20 @@
+echo "Enter Customer Name:"
+read name
+
+echo "Enter Units Consumed:"
+read units
+
+if [ $units -le 100 ]
+then
+    bill=$((units * 2))
+elif [ $units -le 200 ]
+then
+    bill=$(((100 * 2) + ((units - 100) * 3)))
+else
+    bill=$(((100 * 2) + (100 * 3) + ((units - 200) * 5)))
+fi
+
+echo ""
+echo "Customer Name     : $name"
+echo "Units Consumed    : $units"
+echo "Electricity Bill  : ₹$bill"
